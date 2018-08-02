d5f8db3ba0c95b85c58f040ab77191561c18801e
Yonggang Liu <Yonggang.G.Liu@ericsson.com>
2018-08-02 16:55:53 +0800

    Optimize output

--- dos2unix-according-git-status.py
+++ dos2unix-according-git-status.py
@@ -1,9 +1,14 @@
 import os
 
 #os.system(r"git status > git-status-file-list")
+print("\n"+23*"*"+"Files list"+23*"*")
+with open("git-status-file-list", "r") as f:
+    for line in f.readlines():
+        if "new file:" in line or "modified:" in line:
+            print(line.lstrip()[0:-1])
+print("\n"+23*"*"+"Converting files"+23*"*")
 with open("git-status-file-list", "r") as f:
     for line in f.readlines():
         if "new file:" in line or "modified:" in line:
             parts = line.split(':')
-            #print(parts[0].lstrip() + parts[1][0:-1])
             os.system("dos2unix " + parts[1][0:-1])
